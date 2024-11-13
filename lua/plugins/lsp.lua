c3e6e23ca7d73742ab277f8a435103d82475d96a
Dong Ha's MBP <2dongha.lee@gmail.com>
2024-11-13 01:34:32 +0000

    fixed python lsp

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -154,26 +154,10 @@ return {
     local servers = {
       jsonls = {},
       yamlls = {},
+      pyright = {},
 
       -- clangd = {},
       -- gopls = {},
-      pylsp = {
-        settings = {
-          pylsp = {
-            plugins = {
-              pylsp_rope = {
-                enabled = true,
-              },
-              rope_completion = {
-                enabled = true,
-              },
-              rope_rename = {
-                enabled = true,
-              },
-            },
-          },
-        },
-      },
       -- rust_analyzer = {},
       -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
       --
